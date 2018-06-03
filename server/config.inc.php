502acdbc222e388ed56fbf3fcc7a2bb8bf35ae48
Allen <1107806823@qq.com>
2018-06-03 13:04:47 +0800

    注释配置文件

--- server/config.inc.php
+++ server/config.inc.php
@@ -6,26 +6,19 @@
  * Time: 16:28
  */
 
-error_reporting(E_ALL ^ E_NOTICE); //error_reporting(E_ALL ^ E_NOTICE);//显示除去 E_NOTICE 之外的所有错误信息
-define('STORAGE','file');
-define('DOMAIN','http://192.168.79.206:8081');
-define('ONLINE_DIR','/mnt/hgfs/swoole_webim/rooms/');
+error_reporting(E_ALL ^ E_NOTICE);
+//define('STORAGE','file');
+define('DOMAIN', 'http://192.168.79.206:8081');
+//define('ONLINE_DIR','/mnt/hgfs/swoole_webim/rooms/');
 
 #房间配置
-$rooms=[
-    /*'a'=>'A',
-    'b'=>'B',
-    'c'=>'C',
-    'd'=>'D',
-    'e'=>'E',
-    'f'=>'F',*/
-
-    '1'=>'1',
-    '2'=>'2',
-    '3'=>'3',
-    '4'=>'4',
-    '5'=>'5',
-    '6'=>'6',
+$rooms = [
+    '1' => '1',
+    '2' => '2',
+    '3' => '3',
+    '4' => '4',
+    '5' => '5',
+    '6' => '6',
 ];
 
 $redis_conf = [
