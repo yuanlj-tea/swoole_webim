dbb321313d187fade973915ed4a82e5e4d552e54
Allen <1107806823@qq.com>
2018-06-03 12:39:23 +0800

    去除文件函数加载

--- server/hsw_server.php
+++ server/hsw_server.php
@@ -6,11 +6,11 @@ require_once "config.inc.php";
 require_once "emotion.config.php";
 require_once "functions.php";
 require_once "Predis.php";
-require_once "classes/" . STORAGE . "/ChatBase.class.php";
-require_once "classes/" . STORAGE . "/File.class.php";
-require_once "classes/" . STORAGE . "/ChatUser.class.php";
-require_once "classes/" . STORAGE . "/ChatLine.class.php";
-require_once "classes/Chat.class.php";
+//require_once "classes/" . STORAGE . "/ChatBase.class.php";
+//require_once "classes/" . STORAGE . "/File.class.php";
+//require_once "classes/" . STORAGE . "/ChatUser.class.php";
+//require_once "classes/" . STORAGE . "/ChatLine.class.php";
+//require_once "classes/Chat.class.php";
 require_once "classes/hsw.class.php";
 require_once "classes/Room.class.php";
 
